6e439211c45b18698faade39b340abd518e2c3e3
David Puerto Guerrero <dsp5502@gmail.com>
2026-03-05 17:21:54 -0500

    Ajustes launch settings y descomentar MiddleWare

--- zombie-defense/zombie-defense/Middleware/ApiKeyMiddleware.cs
+++ zombie-defense/zombie-defense/Middleware/ApiKeyMiddleware.cs
@@ -4,25 +4,26 @@
     {
         public async Task InvokeAsync(HttpContext context)
         {
-            //if (context.Request.Path.StartsWithSegments("/scalar/v1"))
-            //{
-            //     await next(context);
-            //     return;
-            //}
+            if (context.Request.Path.StartsWithSegments("/scalar") ||
+                context.Request.Path.StartsWithSegments("/openapi"))
+            {
+                await next(context);
+                return;
+            }
 
-            //if (!context.Request.Headers.TryGetValue("X-API-KEY", out var apiKey))
-            //{
-            //    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-            //    await context.Response.WriteAsync("API key is missing.");
-            //    return;
-            //}
-            //var configuredApiKey = configuration.GetValue<string>("ApiXKey");
-            //if (apiKey != configuredApiKey)
-            //{
-            //    context.Response.StatusCode = StatusCodes.Status403Forbidden;
-            //    await context.Response.WriteAsync("Invalid API key.");
-            //    return;
-            //}
+            if (!context.Request.Headers.TryGetValue("X-API-KEY", out var apiKey))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("API key is missing.");
+                return;
+            }
+            var configuredApiKey = configuration.GetValue<string>("ApiXKey");
+            if (apiKey != configuredApiKey)
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync("Invalid API key.");
+                return;
+            }
             await next(context);
         }
     }
